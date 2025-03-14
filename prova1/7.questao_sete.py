679eb2e5f54b9310d1ce9f5d120f66c2390e2b62
Felipex77 <dias20felipe@gmail.com>
2025-03-14 18:44:14 +0000

    valores alterado

--- prova1/7.questao_sete.py
+++ prova1/7.questao_sete.py
@@ -6,11 +6,12 @@ total = quantidade_adquirida * preco_unitario
 
 if quantidade_adquirida <= 5:
  desconto = (total * 2 )/ 100
- print(f"O valor {total:.2f}")
+ print(f"O valor {total:.2f - desconto}")
 elif quantidade_adquirida > 5 and  10:
  desconto = (total * 3)/ 100
- print(f"O valor {total:.2f}")
+ print(f"O valor {total:.2f - desconto}")
 elif quantidade_adquirida > 10:
- desconto =  desconto = (quantidade_adquirida * 5 )/ 100
+ desconto =  (total * 5 )/ 100
+ print(f"O valor {total:.2f - desconto}")
 else:
   print("Opção invalida")
